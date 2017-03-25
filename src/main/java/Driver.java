3e69c7f7d7262d267937a98af501e03f7995af93
michaelkengel <Michael@unknown.local>
2017-03-25 13:16:29 -0400

    JavaPLA

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -17,77 +17,114 @@ import java.util.Scanner;
 @SuppressWarnings("ALL")
 public class Driver {
 
-    private static final Integer NUMBER_OF_TRAINING_POINTS = 20;
-    private static final Double LEARNING_RATE = .01;
-    private static Double[] x = new Double[NUMBER_OF_TRAINING_POINTS];
-    private static Double[] y = new Double[NUMBER_OF_TRAINING_POINTS];
-    private static final Integer MAX_ITERATIONS = 5000;
-    private static Double[] WEIGHTS = new Double[3]; // 2 dimensions and bias
-    private static Integer[] TRUE_VALUE = new Integer[NUMBER_OF_TRAINING_POINTS];
-    private static final Integer TEST_CASES = 1000;
-    private static double avg;
+    private static Scanner kb = new Scanner(System.in);
+    private static final Integer NUMBER_OF_TRAINING_POINTS = 20; // Number of datapoints for training
+    private static Double LEARNING_RATE = .1; // Value by which the weights are adjusted
+    private static Double BIAS_RATE = .05;
+    private static Double[] x = new Double[NUMBER_OF_TRAINING_POINTS]; // Point X cords
+    private static Double[] y = new Double[NUMBER_OF_TRAINING_POINTS]; // Point Y cords
+    private static final Integer MAX_ITERATIONS = 5000; // Max iterations (if no linear seperation is found)
+    private static Double[] W = new Double[3]; // 2 dimensions and bias
+    private static Integer[] TRUE_VALUE = new Integer[NUMBER_OF_TRAINING_POINTS]; // Actual classification
+    private static final Integer TEST_CASES = 1000; // How many test points to be tested
+    private static Double avg; // Decision boundry accuracy on tests
 
+    /*  ZERO  */
     private static final double zeroXmin = -1.0;
     private static final double zeroXmax = 1;
     private static final double zeroYmin = -1.0;
-    private static final double zeroYmax = -.6;
-
+    private static final double zeroYmax = -.4;
+    /*  ONE   */
     private static final double oneXmin = -1;
     private static final double oneXmax = 1;
-    private static final double oneYmin = -.61;
+    private static final double oneYmin = -.41;
     private static final double oneYmax = 1;
-    private static Scanner kb = new Scanner(System.in);
+    /*  SWITCH */
+    private static boolean chartingSwitch = true;
+    private static boolean debug = false;
 
+    /*  METRIC TESTING  */
+    // Add section here for testing learning rates vs average time to boundry found?
+    // Or is this a silly metric?
 
     public static void main(String args[]){
+        ChartFrame trainingPlot, testPlot;
         String go = "y";
-        while(!go.toLowerCase().contains("n")){
-            chartTrainingData(); // Create the data points, chart the data
+        while (!go.toLowerCase().contains("n")) {
+
+            // Generate and chart the training data
+            trainingPlot = new ChartFrame("Learning", chartTrainingData());
+            trainingPlot.pack();
+            trainingPlot.setVisible(chartingSwitch);
+
+            // Train the perfecptron
             perceptron(); // Train the model
-            chartTestPlot(); // Chart the tests
+
+            // Test the decision boundry and plot the tests and their classifications
+            testPlot = new ChartFrame("Testing", chartTestPlot());
+            testPlot.pack();
+            testPlot.setVisible(chartingSwitch);
+            testPlot.setLocation(700, 0); // Offset on side of screen
+
+            // Present run again option
             System.out.println("Run Again?");
             go = kb.nextLine();
+            trainingPlot.setVisible(false); // Hide chart to block multiple windows
+            testPlot.setVisible(false); // Hide chart to block multiple windows
         }
     }
 
     public static void perceptron(){
-        System.out.println("Training on " + NUMBER_OF_TRAINING_POINTS + " training points...");
-        int totalError, iterationIndex = 0;
-        do{
-            totalError = 0;
-            for(int i= 0; i< NUMBER_OF_TRAINING_POINTS ;i++) {
-                int actual = TRUE_VALUE[i]; // Get the actual / expected value for the current node
-                int predicted = 0;
-                if ((double) (x[i]*WEIGHTS[0] + y[i]*WEIGHTS[1] + WEIGHTS[2]) >= 0){
-                    predicted = 1;
+        if (debug){
+            System.out.println("Training on " + NUMBER_OF_TRAINING_POINTS + " training points...");
+        }
+        int updateCount = 0;
+        int totalError, iterationIndex = 0, comp_code=0;// Init variables
+        while(true){
+            totalError = 0; // Each EPOCH set total error to 0
+            for(int i= 0; i< NUMBER_OF_TRAINING_POINTS; i++) { // for each training point...
+                int actual = TRUE_VALUE[i]; // Get the actual binary value for the current node
+                int predicted = 0; // Initialize var
+                if (debug){
+                    System.out.println("Prediction val: " + (x[i]*W[0]) + (y[i]*W[1]) + W[2]);
+                }
+                if ((double) ( (x[i]*W[0]) + (y[i]*W[1]) + W[2]) >= 0){ // Classify function
+                    predicted = 1; // Get the predicted value
                 } else predicted = 0;
                 int error = actual - predicted; //  1 , 0 or -1
-                System.out.println("Point " + x[i] + ","+ y[i] + " || Predicted: " +predicted + " Actual: " +actual + " Error: " + error);
-                WEIGHTS[0] += x[i] * LEARNING_RATE * error; // adjust line if wrong
-                WEIGHTS[1] += y[i] * LEARNING_RATE * error;
-                WEIGHTS[2] += LEARNING_RATE * error;
+                if (debug){
+                    System.out.println("Point " + x[i] + ","+ y[i] + " || Predicted: " + predicted + " Actual: " +actual + " Error: " + error + "Iteration: " + iterationIndex);
+                }
+                if (error != 0){
+                    updateCount++;
+                }
+                W[0] += x[i] * LEARNING_RATE * error; // adjust line if wrong by learning rate
+                W[1] += y[i] * LEARNING_RATE * error; // in positive or negative direction
+                W[2] += error * BIAS_RATE; // Bias, only allow small adjustment
                 if (error != 0){
                     totalError++; // Count # of errors in the epoch
                 }
             }
+            // Break if boundry is found
             if (totalError == 0){
                 System.out.println(" -------\n" +
-                        "Linear Separating Boundary: " + WEIGHTS[0] +"x "+ WEIGHTS[1] +"y "+ WEIGHTS[2] +"b\n" +
+                        "Linear Separating Boundary: " + W[0] +"x "+ W[1] +"y +"+ W[2] +"\n" +
                         "Found at Iteration: " + iterationIndex);
+                System.out.println("Total Line Updates: " + updateCount);
                 break;
             }
+            // Or break if at max iteratons
             if (iterationIndex >= MAX_ITERATIONS){
                 System.out.println("Hit Max Iterations");
+                System.out.println("Final boundry line: " + W[0] + " " + W[1] + " " + W[2]);
                 break;
             }
             iterationIndex++;
-        }while(true);
-
+        }
         System.out.println("Complete.");
-        System.out.println("Final boundry line: " + WEIGHTS[0] + " " + WEIGHTS[1] + " " + WEIGHTS[2]);
     }
 
-    public static void chartTrainingData(){
+    private static JFreeChart chartTrainingData(){
         System.out.println("Building random plot of " + NUMBER_OF_TRAINING_POINTS);
         JFreeChart chart = ChartFactory.createScatterPlot(
                 NUMBER_OF_TRAINING_POINTS + " Training Points", // chart title
@@ -108,14 +145,12 @@ public class Driver {
         domain.setRange(-1.00, 1.0);
         ValueAxis range = xyPlot.getRangeAxis();
         range.setRange(-1.00,1.0);
-        ChartFrame frame = new ChartFrame("Learning", chart);
-        frame.pack();
-        frame.setVisible(true);
+        return chart;
     }
 
-    public static void chartTestPlot(){
+    private static JFreeChart chartTestPlot(){
         JFreeChart chart = ChartFactory.createScatterPlot(
-                 TEST_CASES + " Decision Boundry Tests", // chart title
+                 TEST_CASES + " Decision Boundary Tests", // chart title
                 "", // x axis label
                 "", // y axis label
                 createTestDataset(), // data
@@ -132,17 +167,15 @@ public class Driver {
         domain.setRange(-1.00, 1.0);
         ValueAxis range = xyPlot.getRangeAxis();
         range.setRange(-1.00,1.0);
-        ChartFrame frame = new ChartFrame("Testing", chart);
-        frame.pack();
-        frame.setVisible(true);
+        return chart;
     }
 
     private static XYDataset createLearningDataset(){
 
         for (int i =0; i<2; i++){ // Set random weights
-            WEIGHTS[i] = new Random().doubles(0,.5).findFirst().getAsDouble(); // small initial weights
+            W[i] = new Random().doubles(-1,1).findFirst().getAsDouble(); // small initial weights
         }
-        WEIGHTS[2] = 1.0; // bias
+        W[2] = 1.0; // bias
 
         XYSeriesCollection result = new XYSeriesCollection();
         XYSeries ones = new XYSeries("1 Point");
@@ -167,7 +200,7 @@ public class Driver {
 
     private static XYDataset createTestDataset() {
         XYSeriesCollection dataset = new XYSeriesCollection();
-        XYSeries miss = new XYSeries("Misclassiefied");
+        XYSeries miss = new XYSeries("Misclassified");
         XYSeries ones = new XYSeries("Predicted 1");
         XYSeries zeros = new XYSeries("Predicted 0");
 
@@ -176,7 +209,7 @@ public class Driver {
         for(int i = 0; i < TEST_CASES ; i++){ // TEST 1's
             testX = new Random().doubles(oneXmin,oneXmax).findFirst().getAsDouble(); // 1- class x
             testY = new Random().doubles(oneYmin,oneYmax).findFirst().getAsDouble(); // 1- class y
-            if ((double) (testX*WEIGHTS[0] + testY*WEIGHTS[1] + WEIGHTS[2]) >= 0){ // Expect a 1
+            if ((double) (testX* W[0] + testY* W[1] + W[2]) >= 0){ // Expect a 1
                 ones.add(testX,testY); // accept if >= 0
             } else miss.add(testX,testY); // else miss
 
@@ -185,7 +218,7 @@ public class Driver {
         for(int i = 0; i < TEST_CASES ; i++){ // TEST 0's
            testX = new Random().doubles(zeroXmin,zeroXmax).findFirst().getAsDouble(); // 0- class x
            testY = new Random().doubles(zeroYmin,zeroYmax).findFirst().getAsDouble(); // 0- class y
-            if ((double) (testX*WEIGHTS[0] + testY*WEIGHTS[1] + WEIGHTS[2]) >= 0){ // Expect < 0
+            if ((double) (testX* W[0] + testY* W[1] + W[2]) >= 0){ // Expect < 0
                 miss.add(testX,testY); // miss if 0 or greater
             } else zeros.add(testX,testY); // correct if < 0
         }
